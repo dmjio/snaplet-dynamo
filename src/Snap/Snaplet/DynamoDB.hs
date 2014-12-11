bb28e877604e61bd6fbcc92296e5139209dfc88f
David Johnson <djohnson.m@gmail.com>
2014-12-11 17:40:47 -0600

    initial commit - placeholder for snaplet-dynamodb

---
+++ src/Snap/Snaplet/DynamoDB.hs
@@ -0,0 +1,13 @@
+{-# LANGUAGE OverloadedStrings #-}
+------------------------------------------------------------------------------
+-- |
+-- Module      : Snap.Snaplet.DynamoDB
+-- Copyright   : Vertigo Media Inc. (c) 2014
+-- Maintainer  : djohnson.m@gmail.com
+-- Stability   : experimental
+-- Portability : POSIX
+-- 
+------------------------------------------------------------------------------
+module Snap.Snaplet.DynamoDB where
+
+
